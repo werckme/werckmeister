271dadd61da4e7ffd85db5fea95a5d8f64b773ff
samba <samba@manjaro>
2019-02-03 11:42:35 +0100

    missing files added

---
+++ src/compiler/metaData.cpp
@@ -0,0 +1,9 @@
+#include "metaData.h"
+
+namespace sheet {
+	namespace compiler {
+		using namespace fm;
+		const Ticks VoiceMetaData::DefaultDuration = 1.0_N4;
+		const Ticks VoiceMetaData::DefaultBarLength = 4 * 1.0_N4;
+    }
+}
