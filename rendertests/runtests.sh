7e74ec95c8a8dc943f791f8a254e9831d3852c55
samba <samba@manjaro>
2019-02-13 23:43:03 +0100

    runtests impl

--- rendertests/runtests.sh
+++ rendertests/runtests.sh
@@ -1,7 +1,13 @@
 compiler=../build/sheetc
+refdir=./references
 for x in ../sheets/*.sheet
 do
+  file=$(basename $x).mid
   $compiler $x
-  
-  rm $(basename $x).mid
+  printf "\e[39m$file: \e[31m"
+  ./compare_midi.py $file $file
+  if [ $? -eq 0 ]; then
+    echo -e "\e[92mOK\e[39m"
+    rm $file
+  fi
 done
