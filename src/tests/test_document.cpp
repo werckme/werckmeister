5401467d520bc299116da96de68f6ad624c27b78
samba <samba@manjaro>
2019-02-22 23:17:47 +0100

    document tests added

---
+++ src/tests/test_document.cpp
@@ -0,0 +1,40 @@
+#include <boost/test/unit_test.hpp>
+#include <iostream>
+#include "compiler/error.hpp"
+#include <sheet/Document.h>
+
+
+BOOST_AUTO_TEST_CASE(test_source_manager_add)
+{
+	using namespace sheet;
+	Document doc;
+	auto id1 = doc.addSource(FM_STRING("a"));
+	auto id2 = doc.addSource(FM_STRING("b"));
+	BOOST_CHECK(id1 != id2);
+	BOOST_CHECK(id1 != Event::UndefinedSource);
+	BOOST_CHECK(id2 != Event::UndefinedSource);
+}
+
+BOOST_AUTO_TEST_CASE(test_source_manager_find_by_path)
+{
+	using namespace sheet;
+	Document doc;
+	auto id1 = doc.addSource(FM_STRING("a"));
+	auto id2 = doc.addSource(FM_STRING("b"));
+	auto id3 = doc.addSource(FM_STRING("c"));
+	BOOST_CHECK(doc.findSourceId(FM_STRING("a")) == id1);
+	BOOST_CHECK(doc.findSourceId(FM_STRING("b")) == id2);
+	BOOST_CHECK(doc.findSourceId(FM_STRING("c")) == id3);
+}
+
+BOOST_AUTO_TEST_CASE(test_source_manager_find_by_id)
+{
+	using namespace sheet;
+	Document doc;
+	auto id1 = doc.addSource(FM_STRING("a"));
+	auto id2 = doc.addSource(FM_STRING("b"));
+	auto id3 = doc.addSource(FM_STRING("c"));
+	BOOST_CHECK(doc.findSourcePath(id1) == FM_STRING("a"));
+	BOOST_CHECK(doc.findSourcePath(id2) == FM_STRING("b"));
+	BOOST_CHECK(doc.findSourcePath(id3) == FM_STRING("c"));
+}
