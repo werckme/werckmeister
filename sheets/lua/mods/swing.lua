3a6f6510d70bb354e0985db67aa9cbfb06e6d858
samba <samba@manjaro>
2019-08-19 22:49:43 +0200

    swing impl.

--- sheets/lua/mods/swing.lua
+++ sheets/lua/mods/swing.lua
@@ -1,13 +1,32 @@
 require "lua/com/com"
 
-function isAccent(timeinfo)
+function isLaidBack(timeinfo, args)
     local q = timeinfo.quarterPosition
-    return q - math.floor(q) == 0.5
+    q = q - math.floor(q)
+    if tonumber(args.grid) == 16 then
+        return q == 0.25 or q == 0.75
+    end
+    return q == 0.5
+end
+
+function offset(args)
+    local offset = tonumber(args.offset)
+    if offset == nil then
+        offset = 100
+    end
+    offset = offset / 100
+    local max = 0.25
+    if tonumber(args.grid) == 16 then
+        max = 0.09
+    end
+    return offset * max
 end
 
 function perform(event, args, timeinfo)
-    if isAccent(timeinfo) then
-        event.offset = 0.2
+    args = tokeyvalue(args)
+    if isLaidBack(timeinfo, args) then
+        event.offset = offset(args)
+        event.velocity = event.velocity * 0.5
     end
     return { event }
 end
