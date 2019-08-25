99fad912450f450285b3ed0e72ec0f2d468463d6
samba <samba@FIPS>
2019-08-26 00:02:11 +0200

    tv added

--- sheets/lua/mods/swing.lua
+++ sheets/lua/mods/swing.lua
@@ -1,12 +1,16 @@
 require "lua/com/com"
 
+function compareQuarters(a, b)
+    return math.abs(a-b) <= 0.1
+end
+
 function isLaidBack(timeinfo, args)
     local q = timeinfo.quarterPosition
     q = q - math.floor(q)
     if tonumber(args.grid) == 16 then
-        return q == 0.25 or q == 0.75
+        return compareQuarters(q, 0.25) or compareQuarters(q, 0.75)
     end
-    return q == 0.5
+    return compareQuarters(q, 0.5)
 end
 
 function offset(args)
