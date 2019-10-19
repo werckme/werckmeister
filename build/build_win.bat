87a6f5cb1714189b4b0efbdb3322c94ff15600c5
samba <samba@FIPS>
2019-10-19 17:10:24 +0200

    cmake project renamed/build.bat added

---
+++ build/build_win.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+call "%VS140COMNTOOLS%\vsvars32.bat"
+
+SET PWD="%cd%"
+for %%i in ("%~dp0.") do SET "CWD=%%~fi"
+
+cd %CWD%
+cmake .. -G "Visual Studio 14 2015"
+msbuild werckmeister.sln /p:Configuration=Debug
