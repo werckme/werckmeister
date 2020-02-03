059148425e661535bde23293e97745411a63e6e8
samba <johannes.unger@vstforx.de>
2020-02-04 00:06:10 +0100

    linux build added

--- build/build_linux.sh
+++ build/build_linux.sh
@@ -1,3 +1,3 @@
-BOOST_ROOT=$HOME cmake -DCMAKE_BUILD_TYPE=Release ..
-make sheetc rehearse
-zip werckmeister.zip sheetc rehearse
+export WM_USE_BOOST_STATIC=1
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
