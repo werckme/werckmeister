bf6989327dd3cacb3198e3880e2aeba4f5061698
samba <samba@manjaro>
2020-04-15 17:21:18 +0200

    test for #122 added

--- rendertests/tests/lua/mods/staccato.lua
+++ rendertests/tests/lua/mods/staccato.lua
@@ -4,6 +4,7 @@ local defaultAmountValue = 50
 
 function perform(events, args, timeinfo) 
     args = tokeyvalue(args)
+    local tag = args.forTag
     local amount = defaultAmountValue
     if args.amount ~= nil then
         amount = tonumber(args.amount)
@@ -11,7 +12,9 @@ function perform(events, args, timeinfo)
     local factor = 100 - ((amount/100) * 90)
     factor = factor / 100
     for i, event in pairs(events) do
-        event.duration = event.duration * factor
+        if tag == nil or contains(event.tags, tag) then
+            event.duration = event.duration * factor
+        end
     end
     return events
 end
