aa07135ca3d0b9b088f6091def1a8ae120ca66ca
samba <samba@manjaro>
2020-05-08 09:49:13 +0200

    missing file added

---
+++ src/sheet/Argument.h
@@ -0,0 +1,16 @@
+#ifndef SHEET_ARGUMENT_H
+#define SHEET_ARGUMENT_H
+
+#include <fm/common.hpp>
+
+namespace sheet {
+
+    struct Argument
+    {
+        fm::String value;
+        fm::String name;
+    };
+
+}
+
+#endif
