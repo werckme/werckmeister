12b9c20c868fbf1d9a50a56c71aab901046a69ce
samba <samba@manjaro>
2020-06-02 22:07:36 +0200

    examples fixed

--- src/compiler/commands/SetTempo.cpp
+++ src/compiler/commands/SetTempo.cpp
@@ -1,12 +1,15 @@
 #include "SetTempo.h"
 #include <compiler/context/AContext.h>
 #include <compiler/metaCommands.h>
+#include <iostream>
+
 namespace sheet {
     namespace compiler {
         void SetTempo::execute(AContext* context)
         {
             auto tempoParameter         = parameters[argumentNames.SetTempo.Bpm];
             auto tempoStringValue = tempoParameter.strValue();
+            std::cout << tempoStringValue << std::endl;	
             if (tempoStringValue == SHEET_META__SET_TEMPO_VALUE_HALF) {
                 context->setTempo(context->masterTempo() * 0.5);
                 return;
@@ -19,7 +22,8 @@ namespace sheet {
                 context->setTempo(context->masterTempo());
                 return;
             }			
-            fm::BPM bpm = tempoParameter.value<fm::BPM>();		
+            fm::BPM bpm = tempoParameter.value<fm::BPM>();	
+            
             context->setTempo(bpm);
         }
     }
