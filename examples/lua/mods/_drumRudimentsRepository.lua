e2b4f4c2d68b17585645071896aa52e67835faf8
samba <samba@manjaro>
2020-06-09 00:26:13 +0200

    further rudiments impl

--- examples/lua/mods/_drumRudimentsRepository.lua
+++ examples/lua/mods/_drumRudimentsRepository.lua
@@ -9,13 +9,19 @@ R="R"
 L="L"
 
 -- durations
-local _4   = 1
-local _8   = 1/2
-local _16  = 1/4
-local _32  = 1/8 
-local _4t  = _4 / 3
-local _8t  = _8 / 3
+local _4    = 1
+local _8    = 1/2
+local _16   = 1/4
+local _32   = 1/8 
+local _4t   = _4 / 3
+local _8t   = _8 / 3
+local _16t  = _16 / 3
+local _32t  = _32 / 3
+-- velocity factors
+local ac  = 1.0 -- accent
+local un  = 0.8 -- unaccented
 
+-- create a rudiment sequence event
 local function C(which, duration, velocityFactor)
     if velocityFactor == nil then
         velocityFactor = 1
@@ -23,17 +29,21 @@ local function C(which, duration, velocityFactor)
     return { which=which, duration=duration, velocityFactor = velocityFactor }
 end
 
+
 Rudiments = {
     ["singleStrokeRoll"] =
         { C(R, _4), C(L, _4) },
     ["doubleStrokeRoll"] =
         { C(R, _4), C(R, _4), C(L, _4), C(L, _4) },
+    ["trippleStrokeRoll"] =
+        { C(R, _4), C(R, _4), C(R, _4), C(L, _4), C(L, _4), C(L, _4) },
+    ["fiveStrokeRoll"] =
+        { C(R, _32, un), C(R, _32, un), C(L, _32, un), C(L, _32, un), C(R, _8, ac) },
     ["singleStrokeFour"] =
         { C(R, _4t), C(L, _4t), C(R, _4t), C(L, _4) },
     ["singleStrokeSeven"] =
         { C(R, _8t), C(L, _8t), C(R, _8t), C(L, _8t), C(R, _8t), C(L, _8t), C(R, _4) },
     ["multipleBounceRoll"] =
         { C(R, _32, 1.0), C(R, _32, 0.9), C(R, _32, 0.8), C(R, _32, 0.7),
-          C(L, _32, 1.0), C(L, _32, 0.9), C(L, _32, 0.8), C(L, _32, 0.7)
-    }
+          C(L, _32, 1.0), C(L, _32, 0.9), C(L, _32, 0.8), C(L, _32, 0.7)}
 }
