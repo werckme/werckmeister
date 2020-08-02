98c6814e855d5b492c8f1327ce07a55dbaf3b839
samba <samba@manjaro>
2020-08-02 19:04:31 +0200

    fluidsynth test removed

--- src/sheetplayer_main.cpp
+++ src/sheetplayer_main.cpp
@@ -30,8 +30,6 @@
 #include <fmapp/Funkfeuer.h>
 #include <fmapp/UdpSender.hpp>
 #include <fmapp/NullStringSender.hpp>
-#include <boost/dll.hpp>
-#include <fluidsynth.h>
 
 #ifdef SHEET_USE_BOOST_TIMER
 #include "fmapp/boostTimer.h"
@@ -47,83 +45,11 @@ int startPlayer(std::shared_ptr<PlayerProgramOptions> programOptionsPtr);
 
 funk::UdpSenderPtr _udpSender = nullptr;
 
-
-
-#include <fluidsynth.h>
-#if defined(WIN32)
-#include <windows.h>
-#define sleep(_t) Sleep(_t * 1000)
-#else
-#include <stdlib.h>
-#include <unistd.h>
-#endif
-int test_fluidsynth()
-{
-	boost::dll::shared_library lib("libfluidsynth-2.dll");
-	auto dll_new_fluid_settings			= lib.get<fluid_settings_t* ()>("new_fluid_settings");
-	auto dll_new_fluid_synth			= lib.get<fluid_synth_t* (fluid_settings_t*)>("new_fluid_synth");
-	auto dll_new_fluid_audio_driver		= lib.get<fluid_audio_driver_t* (fluid_settings_t*, fluid_synth_t*)>("new_fluid_audio_driver");
-	auto dll_fluid_synth_sfload			= lib.get<int(fluid_synth_t*, const char*, int)>("fluid_synth_sfload");
-	auto dll_fluid_synth_noteon			= lib.get<int(fluid_synth_t*, int, int, int)>("fluid_synth_noteon");
-	auto dll_fluid_synth_noteoff		= lib.get<int(fluid_synth_t*, int, int)>("fluid_synth_noteoff");
-	auto dll_fluid_synth_program_change = lib.get<int(fluid_synth_t*, int, int)>("fluid_synth_program_change");
-
-
-	fluid_settings_t* settings;
-	fluid_synth_t* synth;
-	fluid_audio_driver_t* adriver;
-	int sfont_id;
-	int i, key;
-	/* Create the settings. */
-	settings = dll_new_fluid_settings();
-	/* Change the settings if necessary*/
-	/* Create the synthesizer. */
-	synth = dll_new_fluid_synth(settings);
-	/* Create the audio driver. The synthesizer starts playing as soon
-	   as the driver is created. */
-	adriver = dll_new_fluid_audio_driver(settings, synth);
-	/* Load a SoundFont and reset presets (so that new instruments
-	 * get used from the SoundFont) */
-	sfont_id = dll_fluid_synth_sfload(synth, "E:\\Users\\samba\\Soundfonts\\FluidR3_GM.sf2", 1);
-	if (sfont_id == FLUID_FAILED)
-	{
-		puts("Loading the SoundFont failed!");
-		goto err;
-	}
-	/* Initialize the random number generator */
-	srand(getpid());
-	dll_fluid_synth_program_change(synth, 0, 16);
-	for (i = 0; i < 12; i++)
-	{
-		/* Generate a random key */
-		key = 60 + (int)(12.0f * rand() / (float)RAND_MAX);
-		/* Play a note */
-		dll_fluid_synth_noteon(synth, 0, key, 80);
-		/* Sleep for 1 second */
-		sleep(1);
-		/* Stop the note */
-		dll_fluid_synth_noteoff(synth, 0, key);
-	}
-err:
-//	/* Clean up */
-	auto dll_delete_fluid_audio_driver = lib.get<void(fluid_audio_driver_t*)>("delete_fluid_audio_driver");
-	auto dll_delete_fluid_synth = lib.get<void(fluid_synth_t*)>("delete_fluid_synth");
-	auto dll_delete_fluid_settings = lib.get<void(fluid_settings_t*)>("delete_fluid_settings");
-	dll_delete_fluid_audio_driver(adriver);
-	dll_delete_fluid_synth(synth);
-	dll_delete_fluid_settings(settings);
-	return 0;
-}
-
-
 int main(int argc, const char** argv)
 {
 	auto programOptionsPtr = std::make_shared<PlayerProgramOptions>();
 
 	try {
-
-		test_fluidsynth();
-
 		programOptionsPtr->parseProgrammArgs(argc, argv);
 		if (programOptionsPtr->isUdpSet()) {
 			auto host = programOptionsPtr->getUdpHostname();
