093c8c6eed299a9f7a86d09a0403e8fd317d4a05
samba <samba@manjaro>
2021-04-09 18:41:31 +0200

    [#187] fade impl * tween class impl * fade impl

--- src/compiler/commands/SetFade.cpp
+++ src/compiler/commands/SetFade.cpp
@@ -1,13 +1,22 @@
 #include "SetFade.h"
 #include <compiler/context/IContext.h>
+#include <fm/tween.h>
+#include <fm/literals.hpp>
 
 namespace sheet {
     namespace compiler {
         void SetFade::execute(IContextPtr  context)
         {
-            throw std::runtime_error("?");
-            // auto setTo = parameters[argumentNames.SetVolume.SetTo].value<double>();
-            // context->setVolume(setTo);
+            using namespace fm;
+            auto from = parameters[argumentNames.SetFade.FadeFrom].value<double>();
+            auto to = parameters[argumentNames.SetFade.FadeTo].value<double>();
+            auto duration = parameters[argumentNames.SetFade.FadeDuration].value<double>();
+            duration *= fm::PPQ;
+            fm::Tween<double, fm::TweenTypeLin> tween(from, to, duration);
+            for (double t=0; t<=duration; t+= double(1.0_N128) ) {
+                auto value = tween.calc(t);
+                context->setVolume(value, fm::Ticks(t));
+            }
         }
 
     }
