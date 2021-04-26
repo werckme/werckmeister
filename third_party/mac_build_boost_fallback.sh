cdee63e9f0d227fb701b037b78ec49f99d6e694e
samba <samba@manjaro>
2021-04-26 22:13:41 +0200

    mac build boost download link set to sourceforge

--- third_party/mac_build_boost_fallback.sh
+++ third_party/mac_build_boost_fallback.sh
@@ -6,8 +6,10 @@
 VERSION=1.72.0
 FILE=boost_1_72_0
 FILENAME=$FILE.tar.gz
-echo Downloading https://dl.bintray.com/boostorg/release/$VERSION/source/$FILENAME
-curl -L https://dl.bintray.com/boostorg/release/$VERSION/source/$FILENAME > $FILENAME
+#url=https://dl.bintray.com/boostorg/release/$VERSION/source/$FILENAME
+url=https://sourceforge.net/projects/boost/files/boost/$VERSION/$FILENAME
+echo Downloading $url
+curl -L $url > $FILENAME
 echo extracting
 tar -xf $FILENAME
 mv $FILE boost
