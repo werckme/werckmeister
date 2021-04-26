8a3c2952b4543bd89a571b0dfdd608b0b8cfb715
Samba Godschynski <johannes.unger@vstforx.de>
2021-04-26 22:29:58 +0200

    Bugfix/69 rest in accomp track has issues (#198)
    * [#69] fixing offset in template caused by a rest
    * instead of just skipping the rendering we render rests into the context
    * but we do it only if the bar contains more than one rest
    
    * broken boost download replaced by sourceforge link
    
    * mac build boost download link set to sourceforge
    
    Co-authored-by: samba <samba@manjaro>

--- rendertests/runtests.sh
+++ rendertests/runtests.sh
@@ -1,17 +1,1 @@
-pwd=$(pwd)
-cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $cwd
-compiler=../build/sheetc
-refdir=./references
-for x in ./tests/*.sheet
-do
-  file=$(basename $x).mid
-  $compiler $x
-  printf "\e[39m$file: \e[31m"
-  python3 compare_midi.py $file $refdir/$file
-  if [ $? -eq 0 ]; then
-    echo -e "\e[92mOK\e[39m"
-    rm $file
-  fi
-done
-cd $pwd
+python3 runtests.py
