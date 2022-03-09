917a59f486bf62f4d767f490d5daa4a1c99ce509
samba <samba@manjaro>
2022-03-09 22:55:42 +0100

    [#268] impl chord selector & tests

--- src/conductor/selectors/Chord.cpp
+++ src/conductor/selectors/Chord.cpp
@@ -1,6 +1,5 @@
 #include "Chord.h"
 
-
 namespace conductor
 {
 	bool Chord::isMatch(const documentModel::ConductionSelector::Arguments &arguments, const EventWithMetaInfo &evm) const
@@ -8,23 +7,16 @@ namespace conductor
 		const auto &midiEvent = *evm.noteOn;
 		for (const auto &arg : arguments)
 		{
-			// auto eventInformation = _eventInformationServer->find(midiEvent);
-			// if (eventInformation->chordRenderInfo.get() == nullptr) {
-			// 	continue;
-			// }
-			// const auto& chordInfos = *eventInformation->chordRenderInfo;
-			// auto absolutePitches = voicingStrategy.get(chordInfos.chordEvent, chordInfos.chordDef, { arg.pitch }, compiler::TimeInfo());
-			// if (absolutePitches.empty()) 
-			// {
-			// 	continue;
-			// }
-			// auto renderedArgumentDegree = absolutePitches.front();
-			// int midiPitchOfRendredDegree = compiler::MidiContext::toMidiPitch(renderedArgumentDegree);
-			// bool isSamePitchRegardlessOctave = midiPitchOfRendredDegree == (int)midiEvent.parameter1();
-			// if (isSamePitchRegardlessOctave) 
-			// {
-			// 	return true;
-			// }
+			auto eventInformation = _eventInformationServer->find(midiEvent);
+			if (eventInformation->chordRenderInfo.get() == nullptr) {
+				continue;
+			}
+			auto chordInfos = eventInformation->chordRenderInfo;
+			const auto eventChordName = chordInfos->chordEvent.stringValue;
+			if (eventChordName == arg.name) 
+			{
+				return true;
+			}
 		}
 		return false;
 	}
