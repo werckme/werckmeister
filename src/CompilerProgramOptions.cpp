afd6b84fbca26a1639a3b308e5e16c1ef9ff4046
Johannes Unger <unger@digitalklang.de>
2022-05-11 22:38:37 +0200

    create-debug-symbols option added

--- src/CompilerProgramOptions.cpp
+++ src/CompilerProgramOptions.cpp
@@ -10,16 +10,33 @@
 #define ARG_DEBUG "debug"
 #define ARG_BEGIN "begin"
 #define ARG_END "end"
+#define ARG_CREATE_DEBUG_SYMBOLS "create-debug-symbols"
 
 void CompilerProgramOptions::parseProgrammArgs(size_t argc, const char **argv)
 {
     namespace po = boost::program_options;
-    optionsDescription.add_options()(ARG_HELP, "produce help message")(ARG_INPUT, po::value<std::string>(), "input file")(ARG_OUTPUT, po::value<std::string>(), "output file")(ARG_MODE, po::value<std::string>(), "mode: normal | json | validate; in JSON mode the input and output will be implemented using JSON strings. The input JSON has to be Base64 encoded. \
-Validate mode checks for errors and returns the validation result as json object.")(ARG_NOMETA, "dosen't render midi meta events like track name or tempo")(ARG_VERSION, "prints the werckmeister version")(ARG_VERBOSE, "prints further informations to the output")(ARG_DEBUG, "prints debug informations to the output")(ARG_BEGIN, po::value<double>(), "start postition in quarter notes. E.g.: 1.2")(ARG_END, po::value<double>(), "end postition in quarter notes. E.g.: 1.2");
+    optionsDescription.add_options()
+    (ARG_HELP, "produce help message")
+    (ARG_INPUT, po::value<std::string>(), "input file")
+    (ARG_OUTPUT, po::value<std::string>(), "output file")
+    (ARG_MODE, po::value<std::string>(), "mode: normal | json | validate; in JSON mode the input and output will be implemented using JSON strings. The input JSON has to be Base64 encoded. \
+Validate mode checks for errors and returns the validation result as json object.")
+    (ARG_CREATE_DEBUG_SYMBOLS, "adds debug infos to the json output")
+    (ARG_NOMETA, "dosen't render midi meta events like track name or tempo")
+    (ARG_VERSION, "prints the werckmeister version")
+    (ARG_VERBOSE, "prints further informations to the output")
+    (ARG_DEBUG, "prints debug informations to the output")
+    (ARG_BEGIN, po::value<double>(), "start postition in quarter notes. E.g.: 1.2")
+    (ARG_END, po::value<double>(), "end postition in quarter notes. E.g.: 1.2");
     po::positional_options_description p;
     p.add(ARG_INPUT, -1);
     po::store(po::command_line_parser((int)argc, argv).options(optionsDescription).positional(p).run(), variables);
     po::notify(variables);
+}
+
+bool CompilerProgramOptions::isGenerateDebugSymbolsSet() const 
+{
+    return !!variables.count(ARG_CREATE_DEBUG_SYMBOLS);
 }
 
 void CompilerProgramOptions::printHelpText(std::ostream &os)
--- src/CompilerProgramOptions.h
+++ src/CompilerProgramOptions.h
@@ -25,6 +25,7 @@ struct CompilerProgramOptions : public virtual ICompilerProgramOptions
 	virtual double getBegin() const override;
 	virtual bool isEndSet() const override;
 	virtual double getEnd() const override;
+	virtual bool isGenerateDebugSymbolsSet() const;
 	typedef boost::program_options::variables_map Variables;
 	typedef boost::program_options::options_description OptionsDescription;
 	OptionsDescription optionsDescription;
