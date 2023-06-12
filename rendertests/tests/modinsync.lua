fa4a075263df696773dce45326a16275310425bf
samba <samba@manjaro>
2023-06-12 22:33:31 +0200

    adding test for mod in sync via lua

---
+++ rendertests/tests/modinsync.lua
@@ -0,0 +1,11 @@
+function perform(events, params, timeinfo)
+    return {
+        {
+            ["type"] = "cc",
+            ["ccNr"] = 1,
+            ["ccValue"] = 0,
+            ["offset"] = offset
+        },
+        events[1]
+    }
+end
--- src/compiler/commands/SetFade.cpp
+++ src/compiler/commands/SetFade.cpp
@@ -8,7 +8,7 @@ namespace compiler
     void SetFade::execute(IContextPtr context)
     {
         auto from = parameters[argumentNames.SetFade.FadeFrom].value<double>();
-        auto to = parameters[argumentNames.SetFade.FadeTo].value<double>();
+        auto to = parameters[argumentNames.SetFade.FadeTo].value<double>() / 100.0 * 127.0;
         auto duration = parameters[argumentNames.SetFade.FadeDuration].value<double>();
         auto curveType = parameters[argumentNames.SetFade.Curve].value<com::String>();
         Base::fade(context, 11, from, to, duration, curveType);
