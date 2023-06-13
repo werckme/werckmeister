c04bafd9b08153f461c775043240c06ee959b396
Johannes Unger <unger@digitalklang.de>
2023-06-13 07:42:53 +0200

    impl integra7 tone switch

--- rendertests/tests/215luaExtensionSysexSupport.lua
+++ rendertests/tests/215luaExtensionSysexSupport.lua
@@ -3,7 +3,8 @@ function execute(params, timeinfo)
     return {
        {
             ["type"] = "sysex",
-            ["sysexData"]  = {72, 97, 108, 108, 111, 33, 0}
+            -- integra 7, set guitar tone variation to mute
+            ["sysexData"]  = {0x41, 0x10, 0x00, 0x00, 0x64, 0x12, 0x19, 0x02, 0x00, 0x2B, 0x01, 0x39}
        }
     }
 end
