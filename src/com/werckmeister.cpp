7690059f595c5595e8e54ae25201e5e6076bda8f
Johannes Unger <unger@digitalklang.de>
2024-07-24 00:03:52 +0200

    Wed, Jul 24, 2024 12:03:52 AM

--- src/com/werckmeister.cpp
+++ src/com/werckmeister.cpp
@@ -384,7 +384,7 @@ namespace com
 
 	bool Werckmeister::fileIsSheet(const Path &path) const
 	{
-		return throw std::runtime_error("TODO");//boost::filesystem::extension(path) == ".documentModel";
+		throw std::runtime_error("TODO");//return boost::filesystem::extension(path) == ".documentModel";
 	}
 
 	Werckmeister::~Werckmeister() = default;
