d8bf045e0bc1e3faad4308afa3f2552d9f956e01
samba <samba@manjaro>
2025-11-03 22:29:52 +0100

    adding dll targets

--- src/libwerckmeister.h
+++ src/libwerckmeister.h
@@ -3,7 +3,7 @@
 #if _WIN32
   #define WM_EXPORT __declspec(dllexport)
 #else
-  #define WM_EXPORT
+  #define WM_EXPORT __attribute__((visibility("default")))
 #endif
 
 extern "C" WM_EXPORT void wm_hello();
