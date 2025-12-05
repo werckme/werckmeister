b60ee7dc034f11ee48cbdf5e90c971d8d6031d3e
samba <samba@manjaro>
2025-12-05 23:36:02 +0100

    adding missing files

---
+++ src/conductor/selectors/DurationSelector.cpp
@@ -0,0 +1,34 @@
+#include "DurationSelector.h"
+#include <cmath>
+
+#include <iostream>
+
+namespace
+{
+	const com::Ticks eps = 0.001;
+}
+namespace conductor
+{
+	bool DurationSelector::isMatch(const documentModel::ConductionSelector::Arguments &arguments, const EventWithMetaInfo &evm) const
+	{
+		if (evm.noteOff == nullptr)
+		{
+			return false;
+		}
+		
+		const com::midi::Event &noteOn = *evm.midiEvent;
+        const com::midi::Event &noteOff = *evm.noteOff;
+		auto duration = (noteOff.absPosition() - noteOn.absPosition()) / com::PPQ;
+
+		for (const auto &argument : arguments)
+        {
+            auto valueToMatch = argument.numberValue;
+			auto diff = abs(valueToMatch - duration);
+			if (diff <= eps)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ src/conductor/selectors/ShorterThan.cpp
@@ -0,0 +1,27 @@
+#include "ShorterThan.h"
+#include <cmath>
+
+namespace conductor
+{
+	bool ShorterThan::isMatch(const documentModel::ConductionSelector::Arguments &arguments, const EventWithMetaInfo &evm) const
+	{
+		if (evm.noteOff == nullptr)
+		{
+			return false;
+		}
+		
+		const com::midi::Event &noteOn = *evm.midiEvent;
+        const com::midi::Event &noteOff = *evm.noteOff;
+		auto duration = (noteOff.absPosition() - noteOn.absPosition()) / com::PPQ;
+
+		for (const auto &argument : arguments)
+        {
+            auto valueToMatch = argument.numberValue;
+			if (duration <= valueToMatch)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+}
